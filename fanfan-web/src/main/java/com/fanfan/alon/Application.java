a206c8dd64c4e698f57f25ab7c0042ec98647738
iskkkk <zjl122400>
2018-08-20 19:20:46 +0800

    spring boot2.0.4框架搭建

---
+++ fanfan-web/src/main/java/com/fanfan/alon/Application.java
@@ -0,0 +1,28 @@
+package com.fanfan.alon;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 功能描述:spring boot启动类
+ * @param:
+ * @return:
+ * @auther: zoujiulong
+ * @date: 2018/8/20   18:13
+ */
+
+@EnableScheduling
+@EnableCaching
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(Application.class,args);
+    }
+}
